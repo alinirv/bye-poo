93acaf7ff3ab337c175ec05fe2d4270d49f06a0d
AlexandreStrapasson <alexandre.strapasson1@gmail.com>
2023-11-02 20:41:44 -0300

    Feat: install and implement the react router dom, second button working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import DropOutPage from './pages/DropOutPage';
-//import RegisterPage from './pages/RegisterPage';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import RegisterPage from './pages/RegisterPage';
+import DropOutPage from "./pages/DropOutPage";
 
 
 function App() {
-    return(<DropOutPage/>)
-    //return(<RegisterPage/>)
-      
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<RegisterPage/>}/>
+                <Route path='/Desistentes' element={<DropOutPage/>}/>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,7 +1,14 @@
+import { useNavigate } from 'react-router'
 import '../App.css'
 import InputField from "../components/InputField"
 
+
 function RegisterPage(){
+    const navigate = useNavigate();
+    const goToDropOutPage = () => {
+        navigate('/Desistentes');
+    }
+
     return(
         <div className="Body">
         <h3>Informe os dados do desistente</h3>
@@ -9,7 +16,7 @@ function RegisterPage(){
             <InputField id="txtId" type="text" placeholder="SC0000000" label="Identificação: " />
             <InputField id="txtReason" type="text" placeholder="Razão da desistência" label="Razão" />
             <InputField type="submit" value="Arregou" id="bt1"/><br/> <br/>
-            <InputField type="submit" value="Lista de desistentes" id="bt2"/>
+            <InputField type="submit" value="Lista de desistentes" id="bt2" onClick={goToDropOutPage}/>
         </div>
     )
 }
