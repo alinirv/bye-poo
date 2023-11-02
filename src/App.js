bcd33db890028a85acfbe3c6c6badd5f136fa5a6
AlexandreStrapasson <alexandre.strapasson1@gmail.com>
2023-11-02 16:41:26 -0300

    refactor: developed a folder where store the pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import './App.css'
-import InputField from './components/InputField'
+import RegisterPage from './pages/RegisterPage';
 
 function App() {
-  return (
-    <div className="App">
-      <h3>Informe os dados do desistente</h3>
-      <InputField id="txtName" type="text" placeholder="Nome completo" label="Nome: " />
-      <InputField id="txtId" type="text" placeholder="SC0000000" label="Identificação: " />
-      <InputField id="txtReason" type="text" placeholder="Razão da desistência" label="Razão" />
-      <InputField type="submit" value="Arregou" id="bt1"/>
-    </div>
-  );
+    return(<RegisterPage/>)
+      
 }
 
 export default App;
---
+++ src/pages/RegisterPage.js
@@ -0,0 +1,15 @@
+import InputField from "../components/InputField"
+
+function RegisterPage(){
+    return(
+        <div className="App">
+        <h3>Informe os dados do desistente</h3>
+            <InputField id="txtName" type="text" placeholder="Nome completo" label="Nome: " />
+            <InputField id="txtId" type="text" placeholder="SC0000000" label="Identificação: " />
+            <InputField id="txtReason" type="text" placeholder="Razão da desistência" label="Razão" />
+            <InputField type="submit" value="Arregou" id="bt1"/><br/> <br/>
+            <InputField type="submit" value="Lista de desistentes" id="bt2"/>
+        </div>
+    )
+}
+export default RegisterPage;
