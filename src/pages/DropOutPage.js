b60769e89ebc1cdc47bdde5e4848449b1d72a2eb
AlexandreStrapasson <alexandre.strapasson1@gmail.com>
2023-11-03 15:41:33 -0300

    Style: add a css to the button of the DropOut Page

--- src/pages/DropOutPage.js
+++ src/pages/DropOutPage.js
@@ -1,6 +1,13 @@
+import { useNavigate } from "react-router";
 import styles from "../components/DropOut.module.css"
+import InputField from "../components/InputField";
 
 function DropOutPage(){
+    const navigate = useNavigate();
+    const goToRegisterPage = () => {
+        navigate('/');
+    }
+
     return(
         <div className={styles.MainBody}>
             <h1>Lista de desistentes</h1>
@@ -10,12 +17,7 @@ function DropOutPage(){
             <p>...</p>
             <p>...</p>
             <p>...</p>
-            <p>...</p>
-            <p>...</p>
-            <p>...</p>
-            <p>...</p>
-            <p>...</p>
-            <p>...</p>
+            <InputField type="submit" value="Lista de desistentes" id="bt2" onClick={goToRegisterPage}/>
         </div>
     )
 }
