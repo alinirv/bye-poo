a22118a1927f2c12882f6eadd8339276ba2a5fd2
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-11-04 17:16:42 -0300

    fix: fixed missing onClick in submit button isue

--- src/components/Form/SubmitButton/SubmitButton.js
+++ src/components/Form/SubmitButton/SubmitButton.js
@@ -1,8 +1,8 @@
 import submitButtomStyle from './SubmitButton.module.css'
 
-function SubmitButton({ id, description, onSubmit, form }) {
+function SubmitButton({ id, description, onSubmit, onClick, form }) {
     return (
-        <button id={ id } className={ submitButtomStyle.SubmitButton } onSubmit={ onSubmit } form={ form }>
+        <button id={ id } className={ submitButtomStyle.SubmitButton } onSubmit={ onSubmit } onClick={ onClick } form={ form }>
             { description }
         </button>
     )
