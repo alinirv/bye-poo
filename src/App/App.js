b3bc94c7d12259bb9f90d3694edfd0ee280f4475
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-11-04 10:29:08 -0300

    style: removed semicolons from App.js and added a space before opening bracket and closing bracket of roots

--- src/App/App.js
+++ src/App/App.js
@@ -1,17 +1,17 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import RegisterPage from '../pages/RegisterPage';
-import DropOutPage from "../pages/DropOutPage";
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import RegisterPage from '../pages/RegisterPage'
+import DropOutPage from "../pages/DropOutPage"
 
 
 function App() {
     return(
         <BrowserRouter>
             <Routes>
-                <Route path='/' element={<RegisterPage/>}/>
-                <Route path='/Desistentes' element={<DropOutPage/>}/>
+                <Route path='/' element={ <RegisterPage/> }/>
+                <Route path='/Desistentes' element={ <DropOutPage/> }/>
             </Routes>
         </BrowserRouter>
     );
 }
 
-export default App;
+export default App
