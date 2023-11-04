6a95248031ec505fa541450e5bb5b53d636da6c5
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-11-04 13:34:01 -0300

    refact: improved InputField css rules, but the style itself was kept

--- src/components/Form/InputField/InputField.js
+++ src/components/Form/InputField/InputField.js
@@ -1,10 +1,11 @@
-import inpurtFieldStyle from './InputField.module.css'
+import inputFieldStyle from './InputField.module.css'
 
-export default function InputField({ id, label, placeholder, type, value, onClick}) {
+export default function InputField({ id, label, placeholder, type, value}) {
     return (
-        <>
+        <div className={ inputFieldStyle.InputField }>
             <label htmlFor={ id }>{ label }</label>
-            <input type={ type } id={ id } placeholder={ placeholder } name={ id } value={ value } onClick={onClick} />
-        </>
+            <input type={ type } id={ id } className={ inputFieldStyle.Input } 
+                placeholder={ placeholder } name={ id } value={ value } />
+        </div>
     )
 }
