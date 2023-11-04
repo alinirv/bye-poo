1fb3f8e73966ea197cab8dc464c66c607cec10d3
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-11-04 11:27:04 -0300

    refactor: removed react fragment from SubmitButton

--- src/components/SubmitButton/SubmitButton.js
+++ src/components/SubmitButton/SubmitButton.js
@@ -1,11 +1,7 @@
 import submitButtomStyle from './SubmitButton.module.css'
 
 function SubmitButton({ id, description, onClick }) {
-    return (
-        <>
-            <button id={ id } className={ submitButtomStyle.SubmitButton } onClick={ onClick }>{ description }</button>
-        </>
-    )
+    return <button id={ id } className={ submitButtomStyle.SubmitButton } onClick={ onClick }>{ description }</button>
 }
 
 export default SubmitButton
