17df0c66a0de9559a7d03ba36b7b8ea2aa32088d
lucas-sdalmeida <lucas.lucas.sa@hotmail.com>
2023-11-04 21:43:05 -0300

    revert: deleted RegistrationContext

--- src/pages/RegistrationPage/RegistrationPage.js
+++ src/pages/RegistrationPage/RegistrationPage.js
@@ -3,7 +3,6 @@ import { useNavigate } from 'react-router'
 import registrationPageStyle from './RegisterPage.module.css'
 
 import Form, { FormControl, InputField, SelectionField, SubmitButton } from '../../components/Form'
-import RegistrationContextProvider from './RegistrationContext'
 import PageHeader from '../../components/PageHeader'
 
 
@@ -12,22 +11,20 @@ function RegistrationPage(){
     const goToDropOutPage = () => navigate('/Desistentes')
 
     return(
-        <RegistrationContextProvider>
-            <section className={ registrationPageStyle.RegistrationPage }>
-                <PageHeader title='Registro de desistentes' />
+        <section className={ registrationPageStyle.RegistrationPage }>
+            <PageHeader title='Registro de desistentes' />
 
-                <Form id='registrationForm'>    
-                    <InputField id="txtName" label="Nome: " type="text" placeholder="Nome completo" />
-                    <InputField id="txtId" label="Identificação: " type="text" placeholder="SC0000000" />
-                    <SelectionField id="txtReason" label="Razão" placeholder="Razão da desistência" options={[
-                        { value: 'other', description: 'Outra' }
-                    ]} />
-                </Form>
-                <FormControl formId='registrationForm' buttonDescription='Arregou' />
-                
-                <SubmitButton id="bt2" description="Lista de desistentes" onClick={ goToDropOutPage } />
-            </section>
-        </RegistrationContextProvider>
+            <Form id='registrationForm'>    
+                <InputField id="txtName" label="Nome: " type="text" placeholder="Nome completo" />
+                <InputField id="txtId" label="Identificação: " type="text" placeholder="SC0000000" />
+                <SelectionField id="txtReason" label="Razão" placeholder="Razão da desistência" options={[
+                    { value: 'other', description: 'Outra' }
+                ]} />
+            </Form>
+            <FormControl formId='registrationForm' buttonDescription='Arregou' />
+            
+            <SubmitButton id="bt2" description="Lista de desistentes" onClick={ goToDropOutPage } />
+        </section>    
     )
 }
 
